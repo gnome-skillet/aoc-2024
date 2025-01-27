f603b05e58bd72be49aa2b376d9e5b5214844051
Carlo Cosenza <214245+gnome-skillet@users.noreply.github.com>
2025-01-26 18:02:01 -0800

    feat(day15): solve part b of day 15
    add queue to handle box moves for part b of day 15

--- aoc/src/commands/day15.rs
+++ aoc/src/commands/day15.rs
@@ -1,3 +1,6 @@
+use log::{debug, info};
+use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -22,8 +25,8 @@ pub struct Day15 {
 #[derive(Debug, Default, Copy, Clone, Hash, PartialEq)]
 pub enum Object {
     Robot,
-    BigBoxLeft,
-    BigBoxRight,
+    BoxLeft,
+    BoxRight,
     Box,
     #[default]
     Wall,
@@ -34,20 +37,26 @@ impl Object {
     pub fn new(tick: char) -> Self {
         match tick {
             '.' => Object::Space,
-            '[' => Object::BigBoxLeft,
-            ']' => Object::BigBoxRight,
+            '[' => Object::BoxLeft,
+            ']' => Object::BoxRight,
             'O' => Object::Box,
             '@' => Object::Robot,
             '#' => Object::Wall,
             _ => panic!(),
         }
     }
-}
 
-#[derive(Debug, Copy, Clone, Hash)]
-pub struct Point {
-    row: usize,
-    column: usize,
+    pub fn to_char(&self) -> char {
+        match *self {
+            Object::Space => '.',
+            Object::BoxLeft => '[',
+            Object::BoxRight => ']',
+            Object::Box => 'O',
+            Object::Robot => '@',
+            Object::Wall => '#',
+            _ => panic!(),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -56,7 +65,7 @@ pub struct Grid {
     robot_position: (usize, usize),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct Direction {
     row: i32,
     column: i32,
@@ -73,6 +82,8 @@ pub fn find_robot(grid: &Vec<Vec<Object>>) -> Option<(usize, usize)> {
     None
 }
 
+pub type Point = (usize, usize);
+
 impl Direction {
     pub fn new(tick: char) -> Self {
         match tick {
@@ -86,7 +97,7 @@ impl Direction {
 }
 
 impl Direction {
-    fn next_position(&self, p: (usize, usize)) -> (usize, usize) {
+    fn next_position(&self, p: Point) -> Point {
         let row: i32 = p.0 as i32 + self.row;
         let column: i32 = p.1 as i32 + self.column;
         (row as usize, column as usize)
@@ -104,6 +115,89 @@ impl Grid {
 }
 
 impl Grid {
+    fn show_robot_position(&self) {
+        println!("robot position: {:?}", self.robot_position);
+    }
+
+    fn show_rows(&self) {
+        for row in self.grid.iter() {
+            for x in row.iter() {
+                print!("{}", x.to_char());
+            }
+            println!();
+        }
+    }
+
+    fn vertical_move(&mut self, d: Direction, i: usize) {
+        let mut curr_p: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut visited: HashSet<(usize, usize)> = HashSet::new();
+        let mut steps: Vec<(usize, usize)> = Vec::new();
+        curr_p.push_back(self.robot_position);
+        while let Some((row, column)) = curr_p.pop_front() {
+            if !visited.contains(&(row, column)) {
+                steps.push((row, column));
+                visited.insert((row, column));
+            }
+            let (next_row, next_column) = d.next_position((row, column));
+            match self.grid[next_row][next_column] {
+                Object::Wall => {
+                    return;
+                }
+                Object::Box => {
+                    curr_p.push_back((next_row, next_column));
+                }
+                Object::BoxLeft => {
+                    curr_p.push_back((next_row, next_column));
+                    if self.grid[row][column] != self.grid[next_row][next_column] {
+                        curr_p.push_back((next_row, next_column + 1));
+                    }
+                }
+                Object::BoxRight => {
+                    curr_p.push_back((next_row, next_column));
+                    if self.grid[row][column] != self.grid[next_row][next_column] {
+                        curr_p.push_back((next_row, next_column - 1));
+                    }
+                }
+                Object::Space => {}
+                _ => (),
+            }
+        }
+        while let Some(curr) = steps.pop() {
+            let (next_row, next_column) = d.next_position((curr.0, curr.1));
+            self.grid[next_row][next_column] = self.grid[curr.0][curr.1];
+            self.grid[curr.0][curr.1] = Object::Space;
+        }
+        self.robot_position = d.next_position(self.robot_position);
+    }
+
+    fn horizontal_move(&mut self, d: Direction) {
+        let mut curr_p: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut steps: Vec<(usize, usize)> = Vec::new();
+        curr_p.push_back(self.robot_position);
+        while let Some((row, column)) = curr_p.pop_front() {
+            steps.push((row, column));
+            let (next_row, next_column) = d.next_position((row, column));
+            match self.grid[next_row][next_column] {
+                Object::Wall => {
+                    return;
+                }
+                Object::BoxLeft | Object::BoxRight | Object::Box => {
+                    curr_p.push_back((next_row, next_column));
+                }
+                Object::Space => {
+                    break;
+                }
+                _ => panic!(),
+            }
+        }
+        while let Some(curr) = steps.pop() {
+            let (next_row, next_column) = d.next_position((curr.0, curr.1));
+            self.grid[next_row][next_column] = self.grid[curr.0][curr.1];
+            self.grid[curr.0][curr.1] = Object::Space;
+        }
+        self.robot_position = d.next_position(self.robot_position);
+    }
+
     pub fn move_robot(&mut self, d: Direction) {
         let (robot_row, robot_column) = self.robot_position;
         let (next_row, next_column) = d.next_position(self.robot_position);
@@ -137,10 +231,13 @@ impl Grid {
 
     pub fn sum_boxes(&self) -> usize {
         let mut sumboxes: usize = 0;
-        for row in 0..self.grid.len() {
-            for column in 0..self.grid.len() {
-                if self.grid[row][column] == Object::Box {
-                    sumboxes += row * 100 + column;
+        for row in 1..self.grid.len() {
+            for column in 1..self.grid[row].len() {
+                if self.grid[row][column] == Object::Box
+                    || self.grid[row][column] == Object::BoxLeft
+                {
+                    let curr = row * 100 + column;
+                    sumboxes += curr;
                 }
             }
         }
@@ -148,18 +245,9 @@ impl Grid {
     }
 }
 
-use regex::Regex;
-
 fn parse_row(input: &str) -> IResult<&str, Vec<Object>> {
     let (input, row) = many1(one_of("#.@O"))(input)?;
     let s: String = row.into_iter().collect();
-    //let re = Regex::new(r"(?<space>[\.\#])").unwrap();
-    //let after = re.replace_all(&s, "$space$space");
-    //let re = Regex::new(r"(?<box>O)").unwrap();
-    //let after = re.replace_all(&after, "[]");
-    //let re = Regex::new(r"(?<robot>@)").unwrap();
-    //let after = re.replace_all(&after, "@.");
-    //let after_chars: Vec<char> = after.chars().collect();
     let after_chars: Vec<char> = s.chars().collect();
     let objects: Vec<Object> = after_chars.iter().map(|x| Object::new(*x)).collect::<Vec<Object>>();
     Ok((input, objects))
@@ -200,16 +288,52 @@ pub fn show(grid: &Grid) {
     }
 }
 
+fn double_up(objects: &Vec<Object>) -> Vec<Object> {
+    objects.iter().flat_map(|v| vec![*v, *v]).map(|x| x).collect()
+}
+
 impl CommandImpl for Day15 {
     fn main(&self) -> Result<(), DynError> {
         let blob_string = fs::read_to_string(&self.input)?;
         let Ok((_, (rows, moves))) = parse_challenge(&blob_string) else { todo!() };
-        for row in rows.iter() {
-            println!("row: {:?}", row);
+        let mut grid: Grid = Grid::new(rows);
+        for m in moves.iter() {
+            grid.move_robot(*m);
+        }
+        println!("sum boxes = {:?}", grid.sum_boxes());
+
+        let Ok((_, (rows, _))) = parse_challenge(&blob_string) else { todo!() };
+        let mut rows: Vec<Vec<Object>> = rows.iter().map(|r| double_up(r)).collect();
+        for row in rows.iter_mut() {
+            let mut box_conversion: bool = false;
+            let mut second_robot: bool = false;
+            for v in row.iter_mut() {
+                if *v == Object::Robot {
+                    if second_robot {
+                        *v = Object::Space;
+                        second_robot = false;
+                    } else {
+                        second_robot = true;
+                    }
+                }
+                if *v == Object::Box {
+                    if box_conversion {
+                        *v = Object::BoxRight;
+                        box_conversion = false;
+                    } else {
+                        *v = Object::BoxLeft;
+                        box_conversion = true;
+                    }
+                }
+            }
         }
         let mut grid: Grid = Grid::new(rows);
-        for m in moves.into_iter() {
-            grid.move_robot(m);
+        for (i, m) in moves.iter().enumerate() {
+            if m.column == 0 {
+                grid.vertical_move(*m, i);
+            } else {
+                grid.horizontal_move(*m);
+            }
         }
         println!("sum boxes = {:?}", grid.sum_boxes());
 
